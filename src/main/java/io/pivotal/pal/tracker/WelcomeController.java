1be150416e3ea2dab5bb84276ae6986350584ff8
Jegan <jegan-s@hcl.com>
2018-05-08 09:25:22 -0600

    SpringLab changes.

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,13 +1,19 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
+    String hello=null;
 
+     public WelcomeController(@Value("${WELCOME_MESSAGE}")String message) {
+       hello=message;
+    }
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+        return hello;
     }
+
 }
